5a91ba09ecc38a4f314117dce399d49a40e98741
Vijay Kumar <vijayykumar051@gmail.com>
2025-05-28 22:28:03 +0530

    Update product category page

--- client/src/pages/ProductCategory.jsx
+++ client/src/pages/ProductCategory.jsx
@@ -22,7 +22,7 @@ const ProductCategory = () => {
         </div>
       )}
       {filteredProducts.length > 0 ? (
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6'>
+        <div className='grid grid-cols sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6'>
             {filteredProducts.map((product)=>(
                 <ProductCard key={product._id} product={product}/>
             ))}
